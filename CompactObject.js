85151fb2e59c432714827f08b734eccacf18c5fe
Juan Guillermo Sierra Agreda <juan.sierra@parivedasolutions.com>
2026-01-13 21:06:31 -0500

    chore: day 17

---
+++ CompactObject.js
@@ -0,0 +1,39 @@
+/*Given an object or array obj, return a compact object.
+
+A compact object is the same as the original object, except with keys containing falsy values removed. This operation applies to the object and any nested objects. Arrays are considered objects where the indices are keys. A value is considered falsy when Boolean(value) returns false.
+
+You may assume the obj is the output of JSON.parse. In other words, it is valid JSON.*
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+    if(Array.isArray(obj)){
+        for(let i = 0; i < obj.length; ++i){
+            if(obj[i] && typeof(obj[i]) == 'object'){
+                compactObject(obj[i]);
+                continue;
+            }
+            if(Boolean(obj[i])){
+                continue;
+            }
+            obj.splice(i, 1);
+            --i;
+        }
+        
+    } else {
+        for(let i in obj){
+            if(obj[i] && typeof(obj[i]) == 'object'){
+                compactObject(obj[i]);
+                continue;
+            }
+            if(Boolean(obj[i])){
+                continue;
+            }
+            delete obj[i];
+        }
+    }
+    return obj; 
+};
+
+obj = [null, 0, false, 1]
+console.log(compactObject(obj))
